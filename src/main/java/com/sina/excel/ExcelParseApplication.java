5aa20979af83ccfb84aa3974364280511f251474
zhangbin <zhangbin17@staff.sina.com.cn>
2020-01-17 17:33:08 +0800

    first commit

---
+++ src/main/java/com/sina/excel/ExcelParseApplication.java
@@ -0,0 +1,13 @@
+package com.sina.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelParseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelParseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sina/excel/ExcelParseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sina.excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelParseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
